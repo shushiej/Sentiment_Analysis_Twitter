04282abdc260b9b24e55cf676c01ad14dcec72d2
shushiej <shushruthjoshi@gmail.com>
2019-08-13 11:18:34 +1200

    - Remove tweets relating to retweets RT by user - Stylise the Dash App with twitter color scheme - Dash app ready for deployment

--- tweepy_visualisation.py
+++ tweepy_visualisation.py
@@ -10,7 +10,7 @@ import sqlite3
 # Get Twitter User from DB and convert to Dataframe.
 conn = sqlite3.connect('tweets.db')
 cur = conn.cursor()
-cur.execute("SELECT * FROM tweets where user = 'TheNotoriousMMA'")
+cur.execute("SELECT * FROM tweets where user = 'realDonaldTrump'")
 rows = cur.fetchall()
 df = pd.DataFrame(rows, columns=['user', 'user_id', 'tweets', 'id', 'date_created', 'likes', 'retweet_count', 'len', 'sentiment','subjectivity', 'tweet_date', 'tweet_day_of_week', 'tweet_hour'])
 conn.commit()
@@ -36,132 +36,137 @@ df['least_liked_tweet'] = df[min_idx]['tweets'][:1]
 most_retweets_idx = df.groupby(['tweets'])['retweet_count'].transform(max) == df['retweet_count'].max()
 df['most_retweeted_tweet'] = df[most_retweets_idx]['tweets'][:1]
 
-# Get image of Wordcloud.
-DASH_APP = "Sentiment_Analysis_Twitter"
-STATIC_PREFIX = '/{}/data/img/'.format(DASH_APP)
-image_filename = './data/img/elonmusk.png'
-encoded_img = base64.b64encode(open(image_filename, 'rb').read())
-
 app.layout = html.Div(children = [
-    html.H1(children = '@'+df['user'][0]),
-    html.Img(src='/data/img/elonmusk.png', style={'height': '50%'}),
-    html.H5(children='Most Liked Tweet'),
-    html.P(children = df['most_liked_tweet']),
-    html.H5(children='Least Liked Tweet'),
-    html.P(children = df['least_liked_tweet']),
-    html.H5(children='Most retweeted Tweet'),
-    html.P(children = df['most_retweeted_tweet']),
-    html.H3(children= 'Tweet Metrics'),
-    html.Div(children = [
+    html.H1(children = 'Twitter Analysis: @'+df['user'][0]),
+    html.Div(className="tweet_storm twelve columns", children = [
+        
+        html.Blockquote(className = "twitter-tweet", children = [
+            html.H3(children='Most Liked Tweet'),
+            html.H3(className = "twitter-tweet--handle", children='@'+df['user'][0]),
+            html.P(children=df['most_liked_tweet'])
+        ]),
+        
+        html.Blockquote(className = "twitter-tweet", children = [
+            html.H3(children='Least Liked Tweet'),
+            html.H3(className = "twitter-tweet--handle", children='@'+df['user'][0]),
+            html.P(children=df['least_liked_tweet'])
+        ]),
+       
+        html.Blockquote(className = "twitter-tweet", children = [
+            html.H3(children='Most retweeted Tweet'),
+            html.H3(className = "twitter-tweet--handle", children='@'+df['user'][0]),
+            html.P(children=df['most_retweeted_tweet'])
+        ])
+    ]),
+    
+    html.Div(className = "twelve columns", children = [
         html.Div(children = [
             dcc.Graph(
-        id='num_likes',
-        figure={
-            'data': [
-                go.Bar(
-                    x=df['tweet_day_of_week'],
-                    y=df['likes'],
-                    opacity=0.9,
-                    name='Num Likes'
-                ),
-                go.Bar(
-                    x=df['tweet_day_of_week'],
-                    y=df['retweet_count'],
-                    opacity=0.9,
-                    name='Num Retweets'
-                )
-            ],
-            'layout': go.Layout(
-                hovermode='closest',
-                title='Likes vs Retweets'
-            )
-        }
-        ),
-    ],className = 'six columns'), 
-    html.Div(children = [
-                dcc.Graph(
-        id='num_tweets',
-        figure={
-            'data': [
-                go.Bar(
-                    x=tweet_counts_by_week.index,
-                    y=tweet_counts_by_week['tweets'],
-                    text='Tweets per Day',
-                    opacity=0.6
-                )
-            ],
-            'layout': go.Layout(
-                hovermode='closest',
-                title='Tweets per Week'
-            )
-        }
-    ),
-    ], className = 'six columns'),
-    ]),
-    html.Div(children = [
+                id='num_likes',
+                figure={
+                    'data': [
+                        go.Bar(
+                            x=df['tweet_day_of_week'],
+                            y=df['likes'],
+                            marker = {
+                                'color': '#1DA1F2'
+                            },
+                            name='Num Likes'
+                        ),
+                        go.Bar(
+                            x=df['tweet_day_of_week'],
+                            y=df['retweet_count'],
+                            marker = {
+                                'color': '#F7A52D'
+                            },
+                            name='Num Retweets'
+                        )
+                    ],
+                    'layout': go.Layout(
+                        hovermode='closest',
+                        title='Likes vs Retweets'
+                    )
+                }
+            ),
+        ],className = 'six columns'), 
         html.Div(children = [
-            html.Div(children = [
                 dcc.Graph(
-                    id='num_tweets_by_hour',
+                    id='num_tweets',
                     figure={
                         'data': [
                             go.Bar(
-                                x=df_2['tweet_hour'],
-                                y=df_2['tweets'],
-                                text='Tweets per Hour',
-                                opacity=0.6
+                                x=tweet_counts_by_week.index,
+                                y=tweet_counts_by_week['tweets'],
+                                text='Tweets per Week',
+                                marker = {
+                                    'color': '#1DA1F2'
+                                }
                             )
                         ],
-                'layout': go.Layout(
-                    hovermode='closest',
-                    title='Tweets per Hour'
-                )
-            }
-    ),
-        ], className = 'six columns'),
-        ], className = "offset-by-one")
-    ], className = "ten columns offset-by-one"),
-    html.H3(children= 'Tweet Content Analysis', className = 'twelve columns'),
+                        'layout': go.Layout(
+                            hovermode='closest',
+                            title='Tweets per Week'
+                        )
+                    }
+                ),
+        ],className = 'six columns'),
+
+    ]),
     html.Div(children = [
-        html.Div(children = [
-             dcc.Slider(
-                id='sentiment_polarity',
-                min = -1,
-                max = 1,
-                marks = {
-                    -1: {'label': 'Very Negative', 'style' : {'color' : '#ce2939'}},
-                    -0.5: 'Slightly Negative',
-                    0: 'Neutral',
-                    0.5: 'Slightly Positive',
-                    1: {'label': 'Very Positive', 'style' : {'color' : '#1876fb'}}
-                },
-                value = df['sentiment'].mean(),
-                step = 0.2,
-                included=False
+        dcc.Graph(
+            id='num_tweets_by_hour',
+            figure={
+                'data': [
+                        go.Bar(
+                            x=df_2['tweet_hour'],
+                            y=df_2['tweets'],
+                            text='Tweets per Hour',
+                            marker = {
+                                'color': '#1DA1F2'
+                            }
+                        )],
+                        'layout': go.Layout(
+                            hovermode='closest',
+                            title='Tweets per Hour'
+                        )
+            }
         ),
-        ], className = 'six columns'),
-        html.Div(children = [
-            dcc.Slider(
-                id='subjectivity',
-                min = -1,
-                max = 1,
-                marks = {
-                    -1: 'Very Subjective',
-                    -0.5: 'Slightly Subjective',
-                    0: 'Neutral',
-                    0.5: 'Slightly Objective',
-                    1: 'Very Objective'
-                },
-                value = df['subjectivity'].mean(),
-                step = 0.2,
-                included=False
-            )
-        ], className = 'six columns') 
-    ], className = 'offset-by-one')
-    
-
-])
-
-
+], className = "twelve columns"),
+    html.Div(children = [
+        html.H3(children= 'Tweet Content Analysis'),
+        dcc.Slider(
+            id='sentiment_polarity',
+            min = -1,
+            max = 1,
+            marks = {
+                -1: {'label': 'Very Negative', 'style' : {'color' : '#ce2939'}},
+                -0.5: 'Slightly Negative',
+                0: 'Neutral',
+                0.5: 'Slightly Positive',
+                1: {'label': 'Very Positive', 'style' : {'color' : '#1876fb'}}
+            },
+            value = df['sentiment'].mean(),
+            step = 0.2,
+            included=False
+    ),
+    ], className = 'sliders twelve columns'),
+    html.Div(children = [
+        dcc.Slider(
+            id='subjectivity',
+            min = -1,
+            max = 1,
+            marks = {
+                -1: 'Very Subjective',
+                -0.5: 'Slightly Subjective',
+                0: 'Neutral',
+                0.5: 'Slightly Objective',
+                1: 'Very Objective'
+            },
+            value = df['subjectivity'].mean(),
+            step = 0.2,
+            included=False
+        )
+    ], className = 'sliders twelve columns') 
+], className = 'offset-by-one')
 if __name__ == '__main__':
     app.run_server(debug=True)
